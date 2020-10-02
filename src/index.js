e9384f70575b6b0e72a978714fe76c84777bb6c8
Suraj Thapa <stsurajst@gmail.com>
2020-10-02 09:16:15 +0545

    graphql server setup complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hi');
+import express from 'express';
+// import bodyParser from 'body-parser';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hi!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`Server is ready at http://localhost:4000${server.graphqlPath}`)
+);
