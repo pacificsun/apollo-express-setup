c0abaaec6a4b672e05cc8ba1d2f4312a99755768
Suraj Thapa <stsurajst@gmail.com>
2020-10-03 21:48:42 +0545

    schema and resolvers separated

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import express from 'express';
 // import bodyParser from 'body-parser';
-import { ApolloServer, gql } from 'apollo-server-express';
+import mongoose from 'mongoose';
+import { typeDefs } from '../typeDefs';
+import { resolvers } from '../resolvers';
+import { ApolloServer } from 'apollo-server-express';
 
-const app = express();
+const startServer = async () => {
+  const app = express();
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-const resolvers = {
-  Query: {
-    hello: () => 'hi!',
-  },
+  server.applyMiddleware({ app });
+  //   await mongoose.connect('mongodb://localhost:27017/test', {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   });
+  app.listen({ port: 4000 }, () =>
+    console.log(`Server is ready at http://localhost:4000${server.graphqlPath}`)
+  );
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`Server is ready at http://localhost:4000${server.graphqlPath}`)
-);
+startServer();
